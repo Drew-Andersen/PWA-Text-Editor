f4b0afd6b38ade8444e59a02eb409b56542d994e
Drew-Andersen <andrewtandersen@gmail.com>
2024-08-27 15:40:54 -0400

    PWA Text Editor

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,14 +12,47 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
+  console.log('putDb');
+
+  // Create a connection to the database
+  const jateDB = await openDB('jate', 1);
+
+  // Creates a new transaction && specifies the database && data privleges
+  const tx = jateDB.transaction('jate', 'readwrite');
+
+  // Opens the object store
+  const store = tx.objectStore('jate');
+
+  // Use the put() method and content is passed in
+  const req = store.put({id: 1, value: content});
+
+  // Confirming the request
+  const res = await req;
+  console.log(`Data saved to the database`, res);
 }
 
-// TODO: Add logic for a method that gets all the content from the database
+// Logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
+  console.log("GET from the database");
+
+  // Creates a connection to the database and version we want to use
+  const textDb = await openDB("jate", 1);
+
+  // Creates a new transaction and specify the database and data privileges
+  const tx = textDb.transaction("jate", "readonly");
+
+  // Opens up the desired object store
+  const store = tx.objectStore("jate");
+
+  // The .get() method is used on the store to grab stored data
+  const request = store.get(1);
+
+  // Confirmation of the request
+  const result = await request;
+  console.log("result.value", result);
+  return result?.value;
 }
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,17 +1,31 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// Event handler for the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    // Store the event
+    window.deferredPrompt = event;
 
+    // Remove the 'hidden' class from the btn
+    butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// Implementing a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
 
+    if (!promptEvent) {
+        return;
+    }
+
+    // Propmt
+    promptEvent.prompt();
+
+    butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// A handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    
+    // Clear the prompt
+    window.deferredPrompt = null;
 });
